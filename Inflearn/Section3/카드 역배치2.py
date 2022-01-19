a0d0fa9447d7213ae43f25d3b8c5c60bc0e3d926
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-01-19 15:25:42 +0900

    Update 카드 역배치2.py
    파이썬 서로의 값 변경 방식!

--- Inflearn/Section3/카드 역배치2.py
+++ Inflearn/Section3/카드 역배치2.py
@@ -1,10 +1,9 @@
-import sys
-sys.stdin=open("input.txt", "r")
 a=list(range(21))
 for _ in range(10):
     s, e=map(int, input().split())
+
     for i in range((e-s+1)//2):
-        a[s+i], a[e-i]=a[e-i], a[s+i]
+        a[s+i], a[e-i]=a[e-i], a[s+i] #파이썬 인덱스 서로 교환 방식
 a.pop(0)
 for x in a:
     print(x, end=' ')

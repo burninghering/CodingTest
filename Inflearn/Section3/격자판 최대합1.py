f795be51b3c95f464e2ebc8d3e4ec7159629f9ee
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-01-23 02:22:41 +0900

    Create 격자판 최대합1.py

---
+++ Inflearn/Section3/격자판 최대합1.py
@@ -0,0 +1,38 @@
+n=int(input())
+
+a=[list(map(int,input().split())) for _ in range(n)]
+
+sum=0
+ans=-1
+for i in range(n):
+  for j in range(n):
+    sum+=a[i][j]
+  if sum>ans:
+    ans=sum
+  sum=0
+
+for i in range(n):
+  for j in range(n):
+    sum+=a[j][i]
+  if sum>ans:
+    ans=sum
+  sum=0
+
+i=0
+for j in range(n-1,-1,-1):
+  sum+=a[i][j]
+  i+=1
+
+if sum>ans:
+  ans=sum
+
+sum=0
+i=0
+for j in range(n):
+  sum+=a[i][j]
+  i+=1
+
+if sum>ans:
+  ans=sum
+
+print(ans) 

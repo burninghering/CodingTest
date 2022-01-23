67f5dabc2d7d19eac09d61fdeaae3001a173fa45
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-01-24 00:35:07 +0900

    Update 사과나무(다이아몬드)(x).py

--- Inflearn/Section3/사과나무(다이아몬드)(x).py
+++ Inflearn/Section3/사과나무(다이아몬드)(x).py
@@ -2,13 +2,13 @@ n=int(input())
 a=[list(map(int,input().split())) for _ in range(n)]
 
 res=0
-s=e=n//2
+s=e=n//2 #인덱스의 규칙성을 변수로 표현할 생각을 못했다..!
 
 for i in range(n):
   for j in range(s,e+1):
-    res+=a[i][j] #여기까지만 구현 성공 
+    res+=a[i][j] #설명 후 여기까지만 구현 성공 
   
-  if i<n//2: 
+  if i<n//2: #if문을 사용하면 되는구나!
     s-=1
     e+=1
 

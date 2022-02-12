c54f3bb6927309087ca4c945a93382d41df35143
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-02-13 02:26:16 +0900

    Update 타이타닉2.py

--- Inflearn/Section4/타이타닉2.py
+++ Inflearn/Section4/타이타닉2.py
@@ -1,4 +1,3 @@
-
 from collections import deque
 
 n, limit=map(int, input().split())
@@ -13,11 +12,11 @@ while p:
     if len(p)==1:
         cnt+=1
         break
-    if p[0]+p[-1]>limit:
+    if p[0]+p[-1]>limit: #제일 가벼운 사람, 제일 무거운 사람을 먼저 태우는 게 최선이라고 생각하심
         p.pop()
         cnt+=1
     else:
-        p.popleft()
+        p.popleft() #아니라면 양쪽 사람을 빼주고 배 1+ 
         p.pop()
         cnt+=1
 print(cnt)
